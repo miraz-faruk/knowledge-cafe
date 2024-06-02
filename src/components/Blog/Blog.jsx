526ad0c877b0224f2d6035451aae62d90d89b1f5
mirazfaruk <mirazfaruk@gmail.com>
2024-06-03 03:31:21 +0600

    Added remove blog form bookmark

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import { CiBookmark } from "react-icons/ci";
 
 const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
-    const { title, cover, author_img, reading_time, author, posted_date, hashtag } = blog;
+    const { id, title, cover, author_img, reading_time, author, posted_date, hashtag } = blog;
     return (
         <div className='border-b-2 mb-10 pb-10'>
             <img className='w-full rounded-lg' src={cover} alt={`cover picture of the title ${title}`} />
@@ -21,7 +21,7 @@ const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
             </div>
             <h1 className="text-[40px] font-bold mb-4">{title}</h1>
             <p className='mb-5'><a href="">{hashtag}</a></p>
-            <button className='text-[#6047EC] text-xl font-semibold underline' onClick={() => handleMarkAsRead(reading_time)}>Mark as read</button>
+            <button className='text-[#6047EC] text-xl font-semibold underline' onClick={() => handleMarkAsRead(id, reading_time)}>Mark as read</button>
         </div>
     );
 };
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -10,7 +10,7 @@ const Bookmarks = ({ bookmarks, readingTime }) => {
             <div className='bg-[#1111110D] p-7 rounded-lg'>
                 <h2 className='text-2xl font-bold'>Bookmarked Blogs: {bookmarks.length}</h2>
                 {
-                    bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
+                    bookmarks.map((bookmark, idx) => <Bookmark key={idx} bookmark={bookmark}></Bookmark>)
                 }
             </div>
         </div>
